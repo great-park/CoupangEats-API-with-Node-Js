2acc4476949a8c73a5276b72f2d5025bc315022f
great-park <83508073+great-park@users.noreply.github.com>
2021-10-01 14:11:21 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 const {logger} = require('./winston');
 
-// TODO: 본인의 DB 계정 입력
+
 const pool = mysql.createPool({
     host: 'risingtest.c0ewkxiorgu8.ap-northeast-2.rds.amazonaws.com',
     user: 'risingTest',
