6767182f3b2f8e685e40fbe3bb0f5cf21f55b7b5
great-park <83508073+great-park@users.noreply.github.com>
2021-10-01 14:12:30 +0900

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
-// TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
-// TODO: .gitignore에 추가하는거 앚지 마세요!
+
+// .gitignore에 추가
 module.exports = {
     'jwtsecret' :  'jwt_secret_key_20210802123456789_secret_key_abcdefg_random_key_secret',
 };
